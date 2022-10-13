bcad8c90be65589b32e1d3cd93b7a4a8b889b00c
jccondoric <www.hackdcjcc@hotmail.com>
2022-10-13 01:14:57 -0400

    Birck
    01

--- Source/Arkanoid_Lab04/BallBound.h
+++ Source/Arkanoid_Lab04/BallBound.h
@@ -4,40 +4,36 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Ball.generated.h"
+#include "BallBound.generated.h"
+class UBoxComponent;
+class APaddlePlayerController;
 
-
-class UProjectileMovementComponent;
 
 UCLASS()
-class ARKANOID_LAB04_API ABall : public AActor
+class ARKANOID_LAB04_API ABallBound : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	ABall();
-	
-	virtual void Launch();
-	bool BallLaunched;
-	
+	ABallBound();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-		UStaticMeshComponent* SM_Ball;
+		UBoxComponent* BoxCollision;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-		UProjectileMovementComponent* ProjectileMovement;
+	APaddlePlayerController* PlayerControllerREF;
+
 
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	 
-	UFUNCTION()
-		UStaticMeshComponent* GetBall();
 
+protected:
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndexType, bool bFromSweet, const FHitResult& SweepResult);
 
 };
